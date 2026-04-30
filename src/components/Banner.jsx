8461fb8cdbed0a0993fb5ac1279eb39d7d41ad68
MRrana26 <hafezmasudranamn@gmail.com>
2026-04-30 13:34:04 +0600

    Slider Image Responsive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,8 +8,8 @@ import Image from 'next/image';
 
 const Banner = () => {
     return (
-        <div className='container mx-auto my-10'>
-            <Swiper
+        <div className='my-10'>
+            <Swiper className='w-[90%] md:w-[90%] lg:w-[90%]  mx-auto'
                 modules={[Navigation, Autoplay]}
                 navigation={true}
                 autoplay={{
@@ -19,15 +19,15 @@ const Banner = () => {
                 loop={true}
             >
                 <SwiperSlide>
-                    <Image src={'/tiles_cover_01.png'} alt='Slider 01' width={1920} height={600} className="w-full h-105 rounded-2xl object-cover" />
+                    <Image src={'/tiles_cover_01.png'} alt='Slider 01' width={1920} height={600} className="w-full h-50 md:h-85 lg:h-105 rounded-2xl object-cover" />
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <Image src={'/tiles_cover_02.png'} alt='Slider 02' width={1920} height={600} className="w-full h-105 rounded-2xl object-cover" />
+                    <Image src={'/tiles_cover_02.png'} alt='Slider 02' width={1920} height={600} className="w-full h-50 md:h-85 lg:h-105 rounded-2xl object-cover" />
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <Image src={'/tiles_cover_03.png'} alt='Slider 03' width={1920} height={600} className="w-full h-105 rounded-2xl object-cover" />
+                    <Image src={'/tiles_cover_03.png'} alt='Slider 03' width={1920} height={600} className="w-full h-50 md:h-85 lg:h-105 rounded-2xl object-cover" />
                 </SwiperSlide>
             </Swiper>
 
