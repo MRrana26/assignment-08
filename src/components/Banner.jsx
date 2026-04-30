6c23c60e802976b7e90dbbaf49df9e4d8231eb21
MRrana26 <hafezmasudranamn@gmail.com>
2026-04-30 13:24:19 +0600

    slider added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,36 @@
+"use client"
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import Image from 'next/image';   
 
 const Banner = () => {
     return (
-        <div>
-            Banner section 
+        <div className='container mx-auto my-10'>
+            <Swiper
+                modules={[Navigation, Autoplay]}
+                navigation={true}
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false
+                }}
+                loop={true}
+            >
+                <SwiperSlide>
+                    <Image src={'/tiles_cover_01.png'} alt='Slider 01' width={1920} height={600} className="w-full h-105 rounded-2xl object-cover" />
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <Image src={'/tiles_cover_02.png'} alt='Slider 02' width={1920} height={600} className="w-full h-105 rounded-2xl object-cover" />
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <Image src={'/tiles_cover_03.png'} alt='Slider 03' width={1920} height={600} className="w-full h-105 rounded-2xl object-cover" />
+                </SwiperSlide>
+            </Swiper>
+
         </div>
     );
 };
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -8,14 +8,14 @@ import NavLink from './NavLink';
 const Navbar = () => {
     return (
         <div className='py-3 shadow-md bg-white'>
-            <div className='container mx-auto flex justify-between items-center'>
+            <div className='container mx-auto flex justify-between items-center px-4 md:px-6 lg:px-8'>
 
-                <div className='flex gap-3 items-center'>
-                    <Image src={'/favicon.png'} alt="Logo" width={40} height={40}/>
-                    <h1 className='text-teal-600 text-3xl font-bold    '>Tiles Gallery</h1>
+                <div className='flex gap-2 md:gap-3 items-center'>
+                    <Image src={'/favicon.png'} alt="Logo" width={40} height={40} className='w-8 h-8 md:w-10 md:h-10'/>
+                    <h1 className='text-teal-600 text-xl md:text-2xl lg:text-3xl font-bold'>Tiles Gallery</h1>
                 </div>
 
-                    <ul className='flex gap-4'>
+                    <ul className='hidden md:flex gap-3 lg:gap-4'>
                         <li className='font-semibold'>
                             <NavLink href={'/'}>Home</NavLink>
                         </li>
@@ -28,7 +28,7 @@ const Navbar = () => {
                     </ul>
 
                     <div>
-                        <Link href={'/login'}><Button>Login</Button></Link>
+                        <Link href={'/login'} className='md:size-default'><Button>Login</Button></Link>
                     </div>
                 
             </div>
